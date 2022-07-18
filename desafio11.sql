14b970747585049b0e635308d92fafa080287cc0
eliel <silvaeliel940@gmail.com>
2022-07-18 18:01:01 -0300

    fix req 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,1 @@
-SELECT notes FROM northwind.purchase_orders WHERE length(notes) >= 30 and length(notes) <= 39;
+SELECT notes FROM northwind.purchase_orders WHERE notes BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order #39';
