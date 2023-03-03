a75f79d7f9edb2ba7b825bf1aa005d2d0bddc297
ElielSilva <silvaeliel940@gmail.com>
2023-03-03 00:23:30 -0300

    project complete

--- desafio12.sql
+++ desafio12.sql
@@ -1,1 +1,2 @@
-SELECT submitted_date FROM northwind.purchase_orders where submitted_date like '2006-04-26%';
+SELECT submitted_date FROM northwind.purchase_orders 
+where submitted_date like '2006-04-26%';
